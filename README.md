6b00bd97834f9cced7bb333b6d2b2d1c7b052393
Herman Mak <hermanmak@google.com>
2020-09-02 14:00:54 +0800

    Working

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 # Setup
 1) Provide a service key for this script to call Google Cloud APIs.
 2) Input audio clip can be provided via local path or a Google Cloud Storage bucket ie gs://YOURFILELOCATION
+3) Run `node index.js`
 
 # Test
 1) enable debugging by setting isDebugMode=true
--- index.js
+++ index.js
@@ -4,20 +4,22 @@ const language = require("@google-cloud/language");
 const fs = require("fs");
 const { Console } = require("console");
 
-const isDebugMode = true;
+// Set to true for all logging output
+const isDebugMode = false;
 
 async function main() {
   const projectId = "nlp-stt";
   const keyFilename =
     "/Users/hermanmak/Documents/Dev/nlp-stt-16634c694dd7.json";
-  // Creates client(s)
+  
+    // Creates client(s)
   const speechClient = new speech.SpeechClient({ projectId, keyFilename });
   const languageClient = new language.LanguageServiceClient({
     projectId,
     keyFilename,
   });
 
-  // // 1) Trigger STT
+  // 1) Trigger STT
   const gcsUri = "gs://raw-voice-clip/20200817-173613.flac";
   const encoding = "FLAC";
   const sampleRateHertz = 48000;
@@ -47,13 +49,15 @@ async function main() {
   // 2) Process Speech response
   const [speechResponse] = await operation.promise();
 
-  debugLogger(`JSON response from Cloud STT: ${JSON.stringify(speechResponse, null, 4)}`);
-  var speechRawPayload = speechResponse.results[0].alternatives[0].transcript;
-  var speechWordArray = speechResponse.results[0].alternatives[0].words;
+  debugLogger(
+    `JSON response from Cloud STT: ${JSON.stringify(speechResponse, null, 4)}`
+  );
+  var sttTranscript = speechResponse.results[0].alternatives[0].transcript;
+  var sttWordInfoArray = speechResponse.results[0].alternatives[0].words;
 
   // 3) Trigger NLP
   const document = {
-    content: speechRawPayload,
+    content: sttTranscript,
     type: "PLAIN_TEXT",
   };
 
@@ -62,28 +66,26 @@ async function main() {
     languageRequest
   );
 
-  debugLogger(`JSON response from Cloud NLP: ${JSON.stringify(languageResponse,null,4)}`);
+  debugLogger(
+    `JSON response from Cloud NLP: ${JSON.stringify(languageResponse, null, 4)}`
+  );
 
-  // 5) Construct output and mergetemp payload
+  // 5) Construct unmerged Verint payload
   const output = {};
   var key = "terms"; // This output payload
   output[key] = []; // An empty array JSON
 
-  const wordOnlyArray = [];
+  const sttWordOnlyArray = [];
 
-  speechWordArray.forEach((wordInfo) => {
-    var start =
-      `${wordInfo.startTime.seconds}` +
-      "." +
-      wordInfo.startTime.nanos / 100000000;
-    var end =
-      `${wordInfo.endTime.seconds}` + "." + wordInfo.endTime.nanos / 100000000;
+  sttWordInfoArray.forEach((wordInfo) => {
+    var start = getStartTimeFromCloudSTTWordInfo(wordInfo);
+    var end = getEndTimeFromCloudSTTWordInfo(wordInfo);
 
     var duration = end - start;
     var speaker = wordInfo.speakerTag;
     var best = {
       word: wordInfo.word,
-      score: wordInfo.confidence * 1000,
+      score: getVerintScoreFromConfidence(wordInfo.confidence),
     };
     var alternatives = [];
 
@@ -94,7 +96,7 @@ async function main() {
       best,
       alternatives,
     });
-    wordOnlyArray.push(wordInfo.word);
+    sttWordOnlyArray.push(wordInfo.word);
   });
   debugLogger(`Non merged output is ${JSON.stringify(output, null, 4)}`);
 
@@ -108,15 +110,15 @@ async function main() {
   languageResponse.entities.forEach((entity) => {
     if (entity.name.includes(" ")) {
       var temp = entity.name.split(" ");
-      var firstIndex = wordOnlyArray.indexOf(temp[0]);
+      var firstIndex = sttWordOnlyArray.indexOf(temp[0]);
 
       // Construct the multi word English Entity
       var combineEntity = {
         startTime: getStartTimeFromCloudSTTWordInfo(
-          speechWordArray[firstIndex]
+          sttWordInfoArray[firstIndex]
         ),
         endTime: getEndTimeFromCloudSTTWordInfo(
-          speechWordArray[firstIndex + temp.length]
+          sttWordInfoArray[firstIndex + temp.length]
         ),
         word: entity.name,
         confidence: 100,
@@ -132,48 +134,80 @@ async function main() {
   languageResponse.entities.forEach((entity) => {
     if (
       entity.name.length > 1 &&
-      wordOnlyArray.includes(entity.name) == false
+      sttWordOnlyArray.includes(entity.name) == false
     ) {
-      var temp = entity.name.split("");
-      var firstIndex = wordOnlyArray.indexOf(temp[0]);
+      var chineseEntityWordArray = entity.name.split("");
+      var chineseEntityFirstWordIndex = sttWordOnlyArray.indexOf(
+        chineseEntityWordArray[0]
+      );
+      var averageConfidence = getAverageConfidenceForWordArray(
+        chineseEntityWordArray,
+        sttWordInfoArray,
+        sttWordOnlyArray
+      );
 
       debugLogger(
         "Found " +
-          temp[0] +
+          chineseEntityWordArray[0] +
           " at index " +
-          firstIndex +
-          "should be inside " +
-          JSON.stringify(speechWordArray[firstIndex], null, 4) +
-          " starttime is " +
-          getStartTimeFromCloudSTTWordInfo(speechWordArray[firstIndex]) +
-          " endtime at index " +
-          (firstIndex + temp.length) +
+          chineseEntityFirstWordIndex +
           "should be inside " +
           JSON.stringify(
-            speechWordArray[firstIndex + temp.length - 1],
+            sttWordInfoArray[chineseEntityFirstWordIndex],
+            null,
+            4
+          ) +
+          " starttime is " +
+          getStartTimeFromCloudSTTWordInfo(
+            sttWordInfoArray[chineseEntityFirstWordIndex]
+          ) +
+          " endtime at index " +
+          (chineseEntityFirstWordIndex + chineseEntityWordArray.length) +
+          "should be inside " +
+          JSON.stringify(
+            sttWordInfoArray[
+              chineseEntityFirstWordIndex + chineseEntityWordArray.length - 1
+            ],
             null,
             4
           ) +
           " is " +
           getEndTimeFromCloudSTTWordInfo(
-            speechWordArray[firstIndex + temp.length - 1]
-          )
+            sttWordInfoArray[
+              chineseEntityFirstWordIndex + chineseEntityWordArray.length - 1
+            ]
+          ) +
+          " average confidence of " +
+          averageConfidence
       );
 
       // Construct the multi word Chinese Entity
+      var startTime = getStartTimeFromCloudSTTWordInfo(
+        sttWordInfoArray[chineseEntityFirstWordIndex - 1]
+      );
+      var endTime = getEndTimeFromCloudSTTWordInfo(
+        sttWordInfoArray[
+          chineseEntityFirstWordIndex + chineseEntityWordArray.length - 1
+        ]
+      );
+      var duration = endTime - startTime;
+      var speaker = 0;
+
       var combinedEntity = {
-        startTime: getStartTimeFromCloudSTTWordInfo(
-          speechWordArray[firstIndex - 1]
-        ),
-        endTime: getEndTimeFromCloudSTTWordInfo(
-          speechWordArray[firstIndex + temp.length - 1]
-        ),
-        word: entity.name,
-        confidence: 100,
+        start: startTime,
+        duration: duration,
+        speaker: speaker,
+        best: {
+          word: entity.name,
+          score: getVerintScoreFromConfidence(averageConfidence),
+        },
+        alternatives: [],
       };
       Array.prototype.splice.apply(
         output["terms"],
-        [firstIndex, temp.length].concat(combinedEntity)
+        [chineseEntityFirstWordIndex, chineseEntityWordArray.length].concat(
+          combinedEntity
+        )
       );
     }
   });
@@ -181,24 +215,20 @@ async function main() {
   debugLogger(`Merged output is ${JSON.stringify(output, null, 4)}`);
 }
 
-// This function only prints to console if debug mode is enabled
+/**
+ * A Console log wrapper that takes into account debug enabling.
+ * @param {*} stringToPrint
+ */
 function debugLogger(stringToPrint) {
   if (isDebugMode) {
     console.log(stringToPrint);
   }
 }
 
-// Checks if a entity is an English entity based on presence of a space or an exact match
-function isEnglishEntity(entity, wordOnlyArray) {
-  if (entity.name.includes(" ")) {
-    return true;
-  } else if (entity.length > 1 && wordOnlyArray.includes([entity])) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
+/**
+ * Extract a start time from Cloud STT wordInfo object.
+ * @param {*} wordInfo the object.
+ */
 function getStartTimeFromCloudSTTWordInfo(wordInfo) {
   const temp =
     `${wordInfo.startTime.seconds}` +
@@ -207,10 +237,46 @@ function getStartTimeFromCloudSTTWordInfo(wordInfo) {
   return temp;
 }
 
+/**
+ * Extract a end time from Cloud STT wordInfo object.
+ * @param {} wordInfo
+ */
 function getEndTimeFromCloudSTTWordInfo(wordInfo) {
   return (
     `${wordInfo.endTime.seconds}` + "." + wordInfo.endTime.nanos / 100000000
   );
+}
+
+/**
+ * Calculate an average confidence based on the confidence scores of the individual words.
+ * @param {*} wordArray
+ * @param {*} sttWordInfoArray
+ * @param {*} sttWordOnlyArray
+ */
+function getAverageConfidenceForWordArray(
+  wordArray,
+  sttWordInfoArray,
+  sttWordOnlyArray
+) {
+  var averageConfidence = 0;
+  wordArray.forEach((word) => {
+    debugLogger(
+      "The word " + word + " indexed at " + sttWordOnlyArray.indexOf(word)
+    );
+
+    averageConfidence +=
+      sttWordInfoArray[sttWordOnlyArray.indexOf(word)].confidence /
+      wordArray.length;
+  });
+  return averageConfidence;
+}
+
+/**
+ * Convert Google Cloud AI confidence scores to Verint based confidence score
+ * @param {*} confidence Google Cloud AI confidence scoring float
+ */
+function getVerintScoreFromConfidence(confidence) {
+  return confidence * 1000;
 }
 
 main().catch(console.error);
